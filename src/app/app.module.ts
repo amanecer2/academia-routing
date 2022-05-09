8ee58014a026060f05f1b3d4e287e8297a7d5f2e
amanecer2levi <shachar.levy@guardicore.com>
2022-05-09 17:58:06 +0300

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { UsersComponent } from './users/users.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemsComponent } from './items/items.component';
+import {HomeComponent} from "./home/home.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'users', //localhost/users
+    component: UsersComponent,
+  },
+  {
+    path: 'dashboard',//localhost/dashboard
+    component: DashboardComponent
+  },
+  {
+    path: 'items',
+    component: ItemsComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    DashboardComponent,
+    ItemsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
